942cc8ea82a09545ff1beef15fb4feb5cab42240
DavidPerezGomez <davidperezgo@gmail.com>
2018-05-02 18:14:22 +0200

    añadidas instrucciones para acordarme

--- src/laboratorio5/Main.java
+++ src/laboratorio5/Main.java
@@ -8,16 +8,23 @@ public class Main {
     public static final int NUMBER_OF_ITERATIONS = 100;
     public static final int NUMBER_OF_THREADS = 6;
 
+    // INSTRUCCIONES DE USO:
+    // 1. uno de los clientes inicializa las variables e inicia sus 3 hilos
+    // 2. el otro cliente inicia sus 3 hilos SIN inicializar las variables
+    // 3. ...
+    // 4. profit
+
     public static void main(String[] args) {
         Data mainData = new Data(mode == Data.LOCKING, "main");
-        ThreadA threadA = new ThreadA(mode, NUMBER_OF_ITERATIONS);
-        ThreadB threadB = new ThreadB(mode, NUMBER_OF_ITERATIONS);
-        ThreadC threadC = new ThreadC(mode, NUMBER_OF_ITERATIONS);
+//        ThreadA threadA = new ThreadA(mode, NUMBER_OF_ITERATIONS);
+//        ThreadB threadB = new ThreadB(mode, NUMBER_OF_ITERATIONS);
+//        ThreadC threadC = new ThreadC(mode, NUMBER_OF_ITERATIONS);
         ThreadD threadD = new ThreadD(mode, NUMBER_OF_ITERATIONS);
         ThreadE threadE = new ThreadE(mode, NUMBER_OF_ITERATIONS);
         ThreadF threadF = new ThreadF(mode, NUMBER_OF_ITERATIONS);
 
-        mainData.initializeVariables();
+
+//        mainData.initializeVariables();
         mainData.showInitialValues();
 
 //      new Thread(threadA).start();
@@ -26,6 +33,8 @@ public class Main {
         new Thread(threadD).start();
         new Thread(threadE).start();
         new Thread(threadF).start();
+
+        mainData.showFinalValues();
     }
 
 }
